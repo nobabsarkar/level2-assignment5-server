80b3d4e1fa55037436d8cd5082122fd689cf8e85
nobabsarkar <nobabsarkar2016@gmail.com>
2025-01-22 15:28:03 +0600

    clean user service code

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,7 +1,6 @@
 import { TUser } from "./user.interface";
 import { User } from "./user.model";
 
-// created user
 const createUserIntoDB = async (payload: TUser) => {
   const result = await User.create(payload);
   return result;
